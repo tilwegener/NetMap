f9771af60ca6f23e84db016c7d59d956873ceebf
xoriin <rhys717@gmail.com>
2026-07-05 10:49:15 +1000

    Add managed device type API client support

---
+++ frontend/src/hooks/useDeviceTypes.ts
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+
+import { api, type DeviceTypeOption } from "../api/client";
+import { deviceTypeOptions } from "../constants";
+import { useApiQuery } from "./useApiQuery";
+
+export function useDeviceTypes(accessToken: string | null) {
+  const query = useApiQuery(
+    accessToken ? () => api.listDeviceTypes(accessToken) : null,
+    [accessToken],
+  );
+
+  const options = useMemo<DeviceTypeOption[]>(() => {
+    if (query.data && query.data.length > 0) return query.data;
+    return deviceTypeOptions.map((value) => ({
+      id: null,
+      value,
+      label: value,
+      icon: value === "other" ? "device" : value,
+      is_builtin: true,
+    }));
+  }, [query.data]);
+
+  const values = useMemo(() => options.map((option) => option.value), [options]);
+
+  return {
+    ...query,
+    options,
+    values,
+  };
+}
